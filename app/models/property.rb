885aeed86255caa951a6910a334753c3258d9cf5
misatol8k <misatolkurihara@gmail.com>
2021-01-05 19:11:52 +0900

    駅名が空欄の場合レコードを登録しないよう設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations, allow_destroy: true
+  accepts_nested_attributes_for :nearest_stations, allow_destroy: true,
+  reject_if: proc { |attributes| attributes['station_name'].blank? }
 end
