cfd48705100eea645ea9a75f4cac17c0ee6e2ae1
misatol8k <misatolkurihara@gmail.com>
2021-01-06 10:13:51 +0900

    インデントを修正

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true,
-  reject_if: proc { |attributes| attributes['station_name'].blank? }
+    reject_if: proc { |attributes| attributes['station_name'].blank? }
   validates :property_name, :rent, :address, presence: true
 end
